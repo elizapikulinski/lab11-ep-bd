bba2905d3a6b52a5b1ab90ba8aaa785b81f847de
bri-dee <bdestoppelaire@ufl.edu>
2026-04-08 10:33:32 -0400

    finished partner 2 tests"

--- calculator.py
+++ calculator.py
@@ -20,10 +20,6 @@ def subtract(a,b):
 def multiply(a,b):
     return a * b
 
-def divide(a,b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b/a
 
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
